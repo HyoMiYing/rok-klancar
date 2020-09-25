0b68d58a836e450715b0d3aff52dcb5a075ecab0
Rok Klancar <rklancar@gmail.com>
2020-09-25 21:01:01 +0200

    settings.py's ALLOWED_HOSTS list now gets multiple strings from env variables

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE'in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    ALLOWED_HOSTS = [f'{os.environ["SITENAME"]}', f'www.{os.environ["SITENAME"]}', f'{os.environ["IP_ADDRESS"]}']
 else:
     DEBUG = True
     SECRET_KEY = 'insecure_key_for_dev'
